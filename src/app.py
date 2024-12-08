88cbc8c34104aaccf30896835cec05881e4aaaaf
Edvin Johannes Piirsalu <edvin.piirsalu@gmail.com>
2024-12-08 01:58:32 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Users, Articles, Tags, ArticlesTags
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,117 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = Users.query.all()
+    users = [user.serialize() for user in users]
+    return jsonify(users), 200
+
+
+
+@app.route('/articles', methods=['GET'])
+def get_tags():
+    data = Articles.query.all()
+    data = [el.serialize() for el in data]
+    return jsonify(data), 200
+
+
+@app.route('/articles/<int:id>', methods=['GET'])
+def get_one_article(id):
+    try:
+        data = Articles.query.get(id)
+        if data is None:
+            raise Exception('no hay articulo!')
+        return jsonify(data.serialize())
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+
+# @app.route('/articles/<art_title>', methods=['GET'])
+# def get_one_article_by_title(art_title):
+#     try:
+#         data = Articles.query.filter_by(title=art_title)
+        
+#         if data is None:
+#             raise Exception('error!')
+#         return jsonify(data.serialize())
+#     except Exception as e:
+#         return jsonify({"error": str(e)})    
+    
+
+@app.route('/articles', methods=['POST'])
+def add_article():
+    try:
+        data = request.json
+        if 'content' not in data or 'title' not in data or 'user_id' not in data:
+            raise Exception('faltan datos!')
+        article = Articles(
+            title = data['title'],
+            content = data['content'],                       # <--- puede ser data.get('content'),
+            user_id = data['user_id']
+        )
+        db.session.add(article)
+        db.session.commit()
+        return jsonify(article.serialize()), 201
+        
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)})
+
+
+
+
+@app.route('/articles/<int:id>', methods=['PUT'])
+def update_article(id):
+    try:
+        data = request.json
+        article = Articles.query.get(id)
+        if article is None:
+            raise Exception('no hay articulo!')
+        
+        article.title = data.get('title', article.title),
+        article.content = data.get('content', article.content),                       # <--- puede ser data.get('content'),
+        article.user_id = data.get('user_id', article.user_id)
+
+        db.session.commit()
+        return jsonify(article.serialize()), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)})
+
+
+
+@app.route('/articles/<int:id>', methods=['DELETE'])
+def delete_article(id):
+    try:
+        data = Articles.query.get(id)
+        if data is None:
+            raise Exception('No hay articulo!')
+        db.session.delete(data)
+        db.session.commit()
+        return jsonify({'msg': 'eliminado'})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+
+
+
+@app.route('/tags', methods=['GET'])
+def get_articles():
+    data = Tags.query.all()
+    data = [el.serialize() for el in data]
+    return jsonify(data), 200
+
+
+@app.route('/articlestags', methods=['GET'])
+def get_articles_tags():
+    data = ArticlesTags.query.all()
+    data = [el.serialize() for el in data]
+    return jsonify(data), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
